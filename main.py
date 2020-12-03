9e945d84636894e53b4006eeb54b63d5f8667014
Cheng <977681708@qq.com>
2020-12-03 13:23:23 +0800

    执行顺序调整

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ if __name__ == "__main__":
     lr=LR(X,Y)
 
     beta=lr.best_theta()
+    # for i in range(0,len(beta)):
+    #     print('beta'+str(i)+'=',beta[i,0])
+
+    
+    # print('size of data is',n)
+    # print('num of x is',p)
+
     n=lr.data_num()
     p=lr.x_num()
     n_p_1=n-p-1
@@ -22,27 +29,35 @@ if __name__ == "__main__":
     sigma_=math.sqrt(Q/n_p_1)
     X_t=lr.conversion(lr.X)
     C=lr.inverse(lr.multiplication(X_t,lr.X))
+    for i in range(1,p+1):
+        betai=beta[i,0]
+        sqrtCii=math.sqrt(C[i,i])
+        num3=sigma_*sqrtCii
+        ti=betai/num3
+        absTi=abs(ti)
+        print('|t'+str(i)+'|=',absTi,absTi>1.960266)
 
-    print('回归系数ß的估计为：',pd.DataFrame(beta))
-    print('残差平方和为：',Q)
-    print('离差平方和为：',U)
 
+    # print('Beta=',pd.DataFrame(beta))
+    # print('Q=',Q)
+    # print('U=',U)
+    # print('F=',F_p_np1)
+
+
+    # print('Q=',Q)
+    # print('U=',U)
     num1=n_p_1/p
     num2=U/Q
     F_p_np1=num1*num2
-    print('F=',F_p_np1)
 
 
-    for i in range(1,p+1):
-        betai=beta[i,0]
-        #print('betai=',betai)
-        sqrtCii=math.sqrt(C[i,i])
-        #print(sqrtCii)
-        num3=sigma_*sqrtCii
-        #print(num3)
-        ti=betai/num3
-        absTi=abs(ti)
-        print(absTi)
+    # for i in range(1,p+1):
+    #     betai=beta[i,0]
+    #     sqrtCii=math.sqrt(C[i,i])
+    #     num3=sigma_*sqrtCii
+    #     ti=betai/num3
+    #     absTi=abs(ti)
+    #     print(absTi)
 
         
 
